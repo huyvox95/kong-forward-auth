4e96cda57797cb92373eb14df69fedc8a5639a4f
Huy Võ <huyvox@Kanekis-MacBook-Pro.local>
2022-12-16 13:01:38 +0700

    update

--- src/schema.lua
+++ src/schema.lua
@@ -10,8 +10,8 @@ return {
                 { timeout = { default = 10000, type = "number" }, },
                 { keepalive_timeout = { default = 60000, type = "number" }, },
                 { auth_uri = { required = true, type = "string" }, },
-                { origin_request_headers_to_forward_to_auth = { type = "array", default = {} }, },
-                { auth_response_headers_to_forward = { type = "array", default = {} }, },
+                { origin_request_headers_to_forward_to_auth = { type = "array", elements = { type = "string" }, default = {} }, },
+                { auth_response_headers_to_forward = { type = "array", elements = { type = "string" }, default = {} }, },
         }, }, },
     },
 }
